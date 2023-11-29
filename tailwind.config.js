bc0317a1baf76a763c810693b1fa1ace107a25c6
Agus27111 <agussetiawan.belajar@gmail.com>
2023-11-30 04:45:47 +0700

    new structure HTML

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
         "very-Dark-grayish-blue": "hsl(234, 12%, 34%)",
         "desaturated-Dark-Blue": "hsl(214, 17%, 51%)",
         "grayish-Blue": "hsl(212, 23%, 69%)",
-        "light-Grayish Blue": "hsl(210, 46%, 95%)",
+        "light-Grayish-Blue": "hsl(210, 46%, 95%)",
       },
     },
     plugins: [],
